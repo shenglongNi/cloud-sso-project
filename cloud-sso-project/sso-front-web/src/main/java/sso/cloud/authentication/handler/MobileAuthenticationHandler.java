508aea26534046cf7c86998a9ddda51a2907d0b5
shenglongNi <shenglongNi@163.com>
2018-10-24 17:33:21 +0800

    config authentication manager

---
+++ cloud-sso-project/sso-front-web/src/main/java/sso/cloud/authentication/handler/MobileAuthenticationHandler.java
@@ -0,0 +1,31 @@
+package sso.cloud.authentication.handler;
+
+import java.security.GeneralSecurityException;
+
+import org.jasig.cas.authentication.AbstractAuthenticationHandler;
+import org.jasig.cas.authentication.Credential;
+import org.jasig.cas.authentication.HandlerResult;
+import org.jasig.cas.authentication.PreventedException;
+import org.jasig.cas.authentication.UsernamePasswordCredential;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MobileAuthenticationHandler extends AbstractAuthenticationHandler {
+
+	@Override
+	public HandlerResult authenticate(Credential credential)
+			throws GeneralSecurityException, PreventedException {
+		
+		return null;
+	}
+
+	@Override
+	public boolean supports(Credential credential) {
+		if(credential instanceof UsernamePasswordCredential) {
+			return true;
+		}
+		
+		return false;
+	}
+
+}
