0d23a0c6641585bca091b627c2397a7253b9a81a
shenglongNi <shenglongNi@163.com>
2018-10-29 23:20:15 +0800

    db config

---
+++ cloud-sso-project/sso-service-api/src/main/java/cloud/sso/domain/User.java
@@ -0,0 +1,26 @@
+package cloud.sso.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_info")
+public class User {
+
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String username;
+	
+	private Integer age;
+	
+	private String address;
+	
+}
+	
