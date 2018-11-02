b14d013fde924bd9c30aa72224f52da805301bd3
shenglongNi <shenglongNi@163.com>
2018-11-02 13:31:48 +0800

    sss

--- cloud-sso-project/sso-service-api/src/main/java/cloud/sso/repository/UserMobileRelatedRepository.java
+++ cloud-sso-project/sso-service-api/src/main/java/cloud/sso/repository/UserMobileRelatedRepository.java
@@ -1,8 +1,6 @@
 package cloud.sso.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import cloud.sso.domain.UserMobileRelated;
 
