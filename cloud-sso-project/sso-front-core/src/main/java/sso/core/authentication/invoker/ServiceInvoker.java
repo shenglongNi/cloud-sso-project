5634c8a6144dd8538a66d25bb471c19d31d36bc2
shenglongNi <shenglongNi@163.com>
2018-11-07 17:57:50 +0800

    resolve confict

--- cloud-sso-project/sso-front-core/src/main/java/sso/core/authentication/invoker/ServiceInvoker.java
+++ cloud-sso-project/sso-front-core/src/main/java/sso/core/authentication/invoker/ServiceInvoker.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package sso.core.authentication.invoker;
 
 import java.util.Map;
@@ -25,31 +24,3 @@ public class ServiceInvoker implements IServiceInvoker{
 	}
 	
 }
-=======
-package sso.core.authentication.invoker;
-
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import sso.core.authentication.invoker.spi.IServiceInvoker;
-import cloud.sso.domain.ResultData;
-import cloud.sso.user.service.api.IUserServiceFeignClient;
-
-@Service
-public class ServiceInvoker implements IServiceInvoker{
-
-	@Autowired
-	private IUserServiceFeignClient userServiceFacade;
-	
-	public Long getUserIdByMobile(String mobile) {
-		 ResultData resultData = userServiceFacade.getUserIdByLoginId(mobile);
-		 if(resultData != null) {
-			 return Long.valueOf((String)resultData.getData().get("userId"));
-		 }
-		 return null;
-	}
-	
-}
->>>>>>> cloud_sso_branch
