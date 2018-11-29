5999a388f1fa788bbf24734bc342e8b786812baa
shenglongNi <shenglongNi@163.com>
2018-11-29 17:27:27 +0800

    filter config

--- cloud-sso-project/scenes-A/src/main/java/client/config/FilterConfig.java
+++ cloud-sso-project/scenes-A/src/main/java/client/config/FilterConfig.java
@@ -1,5 +1,7 @@
 package client.config;
 
+import java.util.Map;
+
 import org.jasig.cas.client.authentication.AuthenticationFilter;
 import org.jasig.cas.client.session.SingleSignOutFilter;
 import org.jasig.cas.client.validation.Cas10TicketValidationFilter;
@@ -7,6 +9,8 @@ import org.jasig.cas.client.validation.Cas10TicketValidator;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.common.collect.Maps;
 
 @Configuration
 public class FilterConfig {
@@ -20,29 +24,27 @@ public class FilterConfig {
 		FilterRegistrationBean filterRegistrationBean = new  FilterRegistrationBean();
 		filterRegistrationBean.setFilter(new SingleSignOutFilter());
 		filterRegistrationBean.addInitParameter("casServerUrlPrefix", "http://localhost:8080/idm/");
-		filterRegistrationBean.addInitParameter("serverName", "http://localhost:2001/sceneA/");
+		filterRegistrationBean.addInitParameter("service", "http://localhost:2001/sceneA/");
 		filterRegistrationBean.setOrder(3);
 		filterRegistrationBean.addUrlPatterns("/*");
 		return filterRegistrationBean;
 	}
 	
 	@Bean
-	public AuthenticationFilter authenticationFilter() {
+	public FilterRegistrationBean authenticationFilter() {
 		FilterRegistrationBean filterRegistrationBean = new  FilterRegistrationBean();
 		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
 		filterRegistrationBean.setFilter(authenticationFilter);
-		authenticationFilter.setCasServerLoginUrl(LOGIN_URL);
-		authenticationFilter.setServerName(SERVER_NAME);
 		filterRegistrationBean.addInitParameter("service", "http://localhost:2001/sceneA/");
-		filterRegistrationBean.addInitParameter("serverName", "http://localhost:2001/sceneA/");
 		filterRegistrationBean.addInitParameter("casServerUrlPrefix", "http://localhost:8080/idm/");
+		filterRegistrationBean.addInitParameter("casServerLoginUrl", LOGIN_URL);
 		filterRegistrationBean.setOrder(4);
 		filterRegistrationBean.addUrlPatterns("/*");
-		return authenticationFilter;
+		return filterRegistrationBean;
 	}
 	
 	@Bean
-	public Cas10TicketValidationFilter cas10TicketValidationFilter(Cas10TicketValidator validator) {
+	public FilterRegistrationBean cas10TicketValidationFilter(Cas10TicketValidator validator) {
 		FilterRegistrationBean filterRegistrationBean = new  FilterRegistrationBean();
 		Cas10TicketValidationFilter cas10TicketValidationFilter = new Cas10TicketValidationFilter();
 		cas10TicketValidationFilter.setService("http://localhost:2001/sceneA/");
@@ -52,13 +54,13 @@ public class FilterConfig {
 		filterRegistrationBean.addUrlPatterns("/*");
 		filterRegistrationBean.setFilter(cas10TicketValidationFilter);
 		filterRegistrationBean.setOrder(5);
-		return cas10TicketValidationFilter;
+		return filterRegistrationBean;
 	}
 	
 	@Bean
 	public Cas10TicketValidator cas10TicketValidator() {
 		Cas10TicketValidator validator = new Cas10TicketValidator("http://localhost:8080/idm/");
 		return validator;
-	
+
 	}
-	}
+}
